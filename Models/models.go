70f37e27b169cbb94623353a3abc8b5215d9c9d1
Owen Bayley <ojbayley@gmail.com>
2021-10-26 17:00:34 +0100

    models

--- Models/models.go
+++ Models/models.go
@@ -1,17 +1,19 @@
 package models
 
-var Boards []Board
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Board struct {
-	Id      int64  `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-	Tasks   []Task `json:"tasks"`
+	Id      primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title   string             `json:"title,omitempty" bson:"title,omitempty"`
+	Desc    string             `json:"desc,omitempty" bson:"desc,omitempty"`
+	Content string             `json:"content,omitempty" bson:"content,omitempty"`
+	Tasks   []Task             `json:"tasks"`
 }
 
 type Task struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+	Title   string `json:"title,omitempty" bson:"title,omitempty"`
+	Desc    string `json:"desc,omitempty" bson:"desc,omitempty"`
+	Content string `json:"content,omitempty" bson:"content,omitempty"`
 }
